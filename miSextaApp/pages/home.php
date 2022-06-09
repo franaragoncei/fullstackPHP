3dd69e1ae5c4975fc2d80103918423e6bfcd9f62
CEI <admin@41.local>
2022-06-09 18:54:13 +0200

    ajax

--- miSextaApp/pages/home.php
+++ miSextaApp/pages/home.php
@@ -7,6 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <style>
+        #contenedor2 {
+            border: 1px solid red;
+            width: 10rem;
+            height: 10rem;
+            margin-top: 15rem;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -18,7 +27,9 @@
         echo '<button><a href="cerrar_sesion.php">Cerrar Sesión</a></button>';
         echo '<button><a href="editarForm.php">Editar Perfil</a></button>';
         echo '<button id="verPeliculas">Ver Peliculas</button>';
+        echo '<button id="ocultarPeliculas">Ocultar Peliculas</button>';
         echo '<div id="contenedor"></div>';
+        echo '<div id="contenedor2"></div>';
         // include 'misPeliculas.php';
     } else {
         header('Location: ../index.php');
@@ -28,6 +39,9 @@
         // var boton = document.getElementById('verPeliculas')
 
         $('#verPeliculas').click(obtenerPeliculas)
+        $('#ocultarPeliculas').click(function() {
+            $('#contenedor').html('')
+        })
 
         function obtenerPeliculas() {
             $.ajax({
@@ -38,12 +52,20 @@
                 success: function(data) {
                     console.log(data);
                     $('#contenedor').html('')
+                    $('#contenedor2').html('')
                     data.forEach(element => {
                         $('#contenedor').append(
                             '<p>' +
                             element.id +
                             ' -> ' +
                             element.nombre +
+                            '</p>'
+                        )
+                        $('#contenedor2').append(
+                            '<p>' +
+                            element.id +
+                            ' -> ' +
+                            element.cine +
                             '</p>'
                         )
                     })
